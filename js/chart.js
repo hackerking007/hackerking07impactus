72b3faa152e63bc7b16575db36546a7a9bf57710
hackerking007 <ayjain71@gmail.com>
2020-09-03 00:28:12 +0530

    2

---
+++ js/chart.js
@@ -0,0 +1,24 @@
+// For a pie chart
+var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: data,
+    options: options
+});
+// And for a doughnut chart
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: options
+});
+data = {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+};
